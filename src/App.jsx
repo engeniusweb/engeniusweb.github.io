39c7d37d3bd600820447d902573d7213a4577722
Samuel Henry <shenry@visiostack.com>
2022-11-21 10:50:42 -0500

    feat: User selector

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,60 @@
+import { useEffect, useState } from 'react';
+import users from './users.json';
+
 export const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [blogPost, setBlogPost] = useState();
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    fetch('https://engeniusweb.com/wp-json/wp/v2/posts?per_page=1')
+        .then((resp) => resp.json())
+        .then((data) => {
+          setBlogPost(data[0]);
+          setLoading(false);
+        });
+  }, [setLoading]);
+
   return (
-    <div>Hello world!</div>
+      <div className='flex justify-center items-center w-screen h-screen'>
+        <div className='bg-white rounded mx-auto w-96 overflow-hidden'>
+          {user ? (
+              <div>
+                <div className='flex items-center p-2'>
+                  <p className='text-xl'>Hi, {user.name.split(' ')[0]}!</p>
+                  <div className='grow'/>
+                  <button className='text-primary hover:text-primary-200' onClick={() => setUser(undefined)}>
+                    Switch User
+                  </button>
+                </div>
+              </div>
+          ) : (
+              <div className='flex flex-col'>
+                <p className='text-xl text-center border-b py-2'>Who Are You?</p>
+
+                {
+                  users.map((user) => {
+                    return (
+                        <div
+                            key={user.username}
+                            className='flex items-center cursor-pointer gap-2 hover:bg-primary-50 transition-colors p-2'
+                            onClick={() => setUser(user)}
+                        >
+                          <img
+                              className='w-12 h-12 rounded-full'
+                              src={`https://engeniusweb.com/signatures/headshots/${user.username}-headshot.jpg`}
+                          />
+                          <div>
+                            <p>{user.name}</p>
+                            <p className='text-sm'>{user.username}@engeniusweb.com</p>
+                          </div>
+                        </div>
+                    );
+                  })
+                }
+              </div>
+          )}
+        </div>
+      </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import colors from 'tailwindcss/colors';
+
 export default {
   content: [
     './index.html',
@@ -6,7 +8,32 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#db3333'
+        primary: {
+          '50': '#ef7c7c',
+          '100': '#ed6666',
+          '200': '#ea5151',
+          '300': '#e83b3b',
+          '400': '#e52525',
+          '500': '#ce2121',
+          '600': '#b71e1e',
+          '700': '#a01a1a',
+          '800': '#891616',
+          '900': '#731313',
+          DEFAULT: '#e52525'
+        },
+        secondary: {
+          '50': '#7ebef2',
+          '100': '#69b3f0',
+          '200': '#53a8ed',
+          '300': '#3e9deb',
+          '400': '#2892e9',
+          '500': '#2483d2',
+          '600': '#2075ba',
+          '700': '#1c66a3',
+          '800': '#18588c',
+          '900': '#144975',
+          DEFAULT: '#2892e9'
+        }
       }
     }
   }
